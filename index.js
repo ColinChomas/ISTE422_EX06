c4ee493ae927a5b527eb02047f0fe5efdf040f32
colinchomas <colinchomas@msn.com>
2025-03-31 16:47:22 -0400

    updated index

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
     res.send('Hallo Welt!!');
     });
 
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server listening at http://0.0.0.0:${port}`);
 });
